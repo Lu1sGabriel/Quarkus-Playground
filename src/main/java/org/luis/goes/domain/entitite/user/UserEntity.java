b28c38d65c2293b3976dc2b163f2ce3046734bc1
Luis Gabriel Goés de Santana <luisgabriel533@gmail.com>
2025-04-08 20:37:49 -0300

    fix: Criação do `UserEntity`

---
+++ src/main/java/org/luis/goes/domain/entitite/user/UserEntity.java
@@ -0,0 +1,19 @@
+package org.luis.goes.domain.entitite.user;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class UserEntity extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public UUID id;
+
+    public String name;
+
+
+}
