9caf0a813b5a189c32225993947637c20515c597
Luis Gabriel Goés de Santana <luisgabriel533@gmail.com>
2025-04-08 21:29:17 -0300

    fix: Adição do método para buscar por id

--- src/main/java/org/luis/goes/domain/service/user/UserService.java
+++ src/main/java/org/luis/goes/domain/service/user/UserService.java
@@ -3,11 +3,17 @@ package org.luis.goes.domain.service.user;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 import org.luis.goes.domain.entitite.user.UserEntity;
+import org.luis.goes.domain.exception.ApiException;
 
 import java.util.List;
+import java.util.UUID;
 
 @ApplicationScoped
 public class UserService {
+
+    public UserEntity getById(UUID id) {
+        return UserEntity.<UserEntity>findByIdOptional(id).orElseThrow(() -> new ApiException.NotFound("User not found. "));
+    }
 
     public List<UserEntity> getAll(Integer page, Integer pageSize) {
         return UserEntity.findAll().page(page, pageSize).list();
